e84d14ef376d9bf7f3bae666f9ab49b3de9c5c70
unknown <chap6680@gmail.com>
2020-04-05 07:33:34 -0600

    additional updates

--- start.js
+++ start.js
@@ -1,43 +1,66 @@
 const testData = [
     {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-{name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+    {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+    {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
 ];
 
 const CardList = (props) => (
-<div>
-{testData.map(profile => <Card {...profile}/>)}
-</div>
+	<div>
+  	{props.profiles.map(profile => <Card {...profile}/>)}
+	</div>
 );
 
 class Card extends React.Component {
-render() {
-const profile = this.props;
-return (
-<div className="github-profile">
-  <img src={profile.avatar_url} />
-<div className="info">
-  <div className="name">{profile.name}</div>
-  <div className="company">{profile.company}</div>
-</div>
-</div>
-);
-}
+	render() {
+  	const profile = this.props;
+  	return (
+    	<div className="github-profile">
+    	  <img src={profile.avatar_url} />
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+    	</div>
+    );
+  }
 }
 
+class Form extends React.Component {
+	render() {
+  	return (
+    	<form action="">
+    	  <input type="text" placeholder="GitHub username"/>
+        <button>Add card</button>
+    	</form>
+    );
+  }
+}
 
 class App extends React.Component {
-render() {
-return (
-<div>
-  <div className="header">{this.props.title}</div>
-<CardList />
-</div>
-);
-}	
+  // constructor(props) {
+  // super(props);
+  // this.state = {
+  // profiles: testData,
+  // ]; 
+  //}
+
+   //abvoe will be replaced by the following  
+  state = {
+    profiles: testData,
+  };
+  
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form />
+        <CardList profiles={this.state.profiles} />
+    	</div>
+    );
+  }	
 }
 
 ReactDOM.render(
-<App title="The GitHub Cards App" />,
-mountNode,
+	<App title="The GitHub Cards App" />,
+  mountNode,
 );
